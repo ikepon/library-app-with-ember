e4a5129ef62d676e4bf05468693a43aca252d2b2
ikepon <tatsuyanoheya@gmail.com>
2016-05-24 19:09:47 +0900

    computed.empty を使って空欄を検知 #1

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   emailAddress: '',
 
-  isDisabled: Ember.computed('emailAddress', function() {
-    return this.get('emailAddress') === '';
-  })
+  isDisabled: Ember.computed.empty('emailAddress')
 });
