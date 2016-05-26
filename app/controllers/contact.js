7704321111a68be10cb183cce915d73fd6e8c4d2
ikepon <tatsuyanoheya@gmail.com>
2016-05-26 20:09:57 +0900

    ボタンを押したらメッセージが表示され入力内容がクリアされる #1

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -6,5 +6,14 @@ export default Ember.Controller.extend({
   isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isValidMessage: Ember.computed.gte('message.length', 5),
 
-  isDisabled: Ember.computed.and('isValidEmail', 'isValidMessage')
+  isDisabled: Ember.computed.and('isValidEmail', 'isValidMessage'),
+
+  actions: {
+    sendMessage() {
+      alert(`Send message, email address is in progress: ${this.get('emailAddress')}, message is: ${this.get('message')}`);
+      this.set('responseMessage', "We got your message and we'll get in touch soon");
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
 });
