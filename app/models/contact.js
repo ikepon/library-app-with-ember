9fb5d370a28cf351a4ef25ba75d219f85ac26606
ikepon <tatsuyanoheya@gmail.com>
2016-05-29 16:22:55 +0900

    contact controllerを削除し、contact form を見やすく修正 #1

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,7 +1,13 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
   email: attr('string'),
-  message: attr('string')
+  message: attr('string'),
+
+  isValidEmail: Ember.computed.match('email', /^.+@.+\..+$/),
+  isValidMessage: Ember.computed.gte('message.length', 5),
+
+  isDisabled: Ember.computed.and('isValidEmail', 'isValidMessage')
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('contact');
+  },
+
+  actions: {
+    sendMessage(contact) {
+      const controller = this.get('controller');
+
+      contact.save().then(() => {
+        controller.set('responseMessage', true);
+      });
+    }
   }
 });
