e5e12aa83ea57a6f2a2b7f96991022c699291e4d
ikepon <tatsuyanoheya@gmail.com>
2016-05-30 20:07:46 +0900

    componentを使ってlibrary form を共通化 #1

--- app/models/library.js
+++ app/models/library.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
-  phone: attr('string')
+  phone: attr('string'),
+
+  isValid: Ember.computed.notEmpty('name')
 });
