8792e8ed5095dec442663c89842e0899f8ae745f
ikepon <tatsuyanoheya@gmail.com>
2016-05-31 18:39:22 +0900

    Library model にbooksとの関連を定義 #1

--- app/models/library.js
+++ app/models/library.js
@@ -1,11 +1,14 @@
 import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
+
+  books: hasMany('books'),
 
   isValid: Ember.computed.notEmpty('name')
 });
